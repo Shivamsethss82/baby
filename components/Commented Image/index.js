a2c86bf424208a42ffba8b70f629496e8404ba4c
shivam seth <shivamsethss82@gmail.com>
2024-05-01 11:41:23 +0530

    first commit

---
+++ components/Commented Image/index.js
@@ -0,0 +1,20 @@
+import { Collapse, useDisclosure } from '@chakra-ui/react';
+import Image from 'next/image'; // Import the Image component from next/image
+
+const CommentedImage = ({ src, text }) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <>
+      <div className="flex flex-col items-center bg-pink-100 transition-all duration-300 hover:shadow-lg hover:bg-pink-200 cursor-pointer">
+        {/* Replace img with Image */}
+        <Image className="p-5" src={src} width={250} height={200} onMouseOver={onToggle} onMouseOut={onToggle} alt='' />
+        <Collapse in={isOpen}>
+          <h1 className="text-pink-400 font-bold uppercase pb-2">{text}</h1>
+        </Collapse>
+      </div>
+    </>
+  );
+};
+
+export default CommentedImage;
